8c950f516c97d55868de238cdb27cd45d8cf6de9
Zach <zacharycoss@gmail.com>
2020-10-22 16:04:40 -0500

    adding mongoose

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const 
+    mongoose = require('./schema/mongoose'),
     schema = require('./schema/schema'),
     socket = require('./socket/socket'),
     AWS = require('./aws/aws');
 
 module.exports = {
+    mongoose: mongoose,
     schema: schema,
     S3: AWS.S3,
     SQS: AWS.SQS,
